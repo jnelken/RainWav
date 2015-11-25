2db5d8d6dc02abb560b7003fd17dda85756c553c
Jake Nelken <jnelks@gmail.com>
2015-11-25 16:09:07 -0500

    Add Flex display to search bar.

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -35,43 +35,41 @@ var Nav = React.createClass({
     trackForm = undefined;
   }
 
+  // <li className="searchbar">
+  //   <input type="search" placeholder="Search"/>
+  // </li>
+
     return (
       <div>
         <div className="navbar-container">
           <ul className="navbar group">
-            <div className="navbar-left">
-              <li className="logo">
-                <a href="#"></a>
-              </li>
 
-              <li className="nav-tab">
-                <a href="#">Home</a>
-              </li>
-            </div>
+            <li className="logo">
+              <a href="#"></a>
+            </li>
+            <li className="nav-tab">
+              <a href="#">Home</a>
+            </li>
 
             <div className="flex-container">
-              <li className="searchbar">
-                <input type="search" placeholder="Search"/>
-              </li>
 
-              <div className="navbar-right">
+              <input type="search" placeholder="Search"/>
+
+              <div className="navbar-right group">
                 <li onClick={this.uploadClick}className="nav-tab">
                     <a>Upload</a>
                 </li>
-
                 <li className="nav-tab nav-profile group">
-
                   <a href={'#/' + username}>
                     <img src={this.props.currentUser.avatar} className="avtr" />
                     {username.capitalize()}
                   </a>
                 </li>
-
                 <li className="more" onClick={this.handleLogout}>
                   <a className="dots" href="#/login" ></a>
                 </li>
-
               </div>
+
             </div>
 
           </ul>
